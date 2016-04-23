0b1684bd2462cae92ff81707ba55907960d443d0
MGovier <merlingovier@gmail.com>
2016-04-23 19:39:05 +0100

    Improve in-code doc comments.

--- app/index.js
+++ app/index.js
@@ -1,11 +1,23 @@
+/**
+ * WebWorkers can't be run from the file system for security reasons.
+ * Express is used to locally host the resources.
+ *
+ * @author UP663652
+ */
+
+// Express is an open-source web framework for Node.js.
+// See: http://expressjs.com/
 const express = require('express');
 const port = process.env.PORT || 1337;
 const app = express();
 
+// Host demo resources.
 app.use(express.static(`${__dirname}/public`));
 
+// Return index for all requests.
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
+// Listen to defined port when started.
 app.listen(port, () => console.log('Express started. Navigate to localhost:1337'));
--- app/src/worker.js
+++ app/src/worker.js
@@ -1,57 +1,70 @@
 /**
+ * Individual worker functionality.
+ * Controlled by, and communicates with client.js.
  *
  * @author UP663652
  */
+
+// Uses the SparkMD5 hashing library from https://github.com/satazor/js-spark-md5
+// Written by André Cruz 2016.
 self.importScripts('spark-md5.min.js');
 
+// Variables that will be set by messages when worker created.
 let workerId = undefined;
 let hash = undefined;
 let workerCount = undefined;
 
+// How often to update the main thread on current speed.
 const REPORT_RATE = 80000;
 
 /**
  * Increment the string by the number of workers.
  * As the initial string is tied to the worker ID, this will keep the workers
  * synchronized.
- * @param  {String} attempt String to increment.
- * @return {String}         Result.
+ * @param  {String}   attempt       String to increment.
+ * @return {String}   nextAttempt   Result.
  */
 function increment(attempt) {
-  let ret = attempt;
-  let lastChar = ret.substr(ret.length - 1).charCodeAt(0);
+  let nextAttempt = attempt;
+  let lastChar = nextAttempt.substr(nextAttempt.length - 1).charCodeAt(0);
   let lastCharIncr = lastChar + workerCount;
   const firstCharIncr = lastCharIncr;
   // Start incrementing preceeding characters, or add one to end.
   let i = 1;
   while (lastCharIncr > 122) {
-    if (++i > ret.length) {
-      ret = String.fromCharCode(97).repeat(ret.length) +
+    if (++i > nextAttempt.length) {
+      nextAttempt = String.fromCharCode(97).repeat(nextAttempt.length) +
         String.fromCharCode(97 + (firstCharIncr - 123));
-      return ret;
+      return nextAttempt;
     }
-    lastChar = ret.substr(ret.length - i).charCodeAt(0);
+    lastChar = nextAttempt.substr(nextAttempt.length - i).charCodeAt(0);
     lastCharIncr = lastChar + 1;
   }
-  ret = ret.substr(0, ret.length - i) + String.fromCharCode(lastCharIncr);
+  nextAttempt = nextAttempt.substr(0, nextAttempt.length - i) + String.fromCharCode(lastCharIncr);
   if (i > 2) {
-    ret += String.fromCharCode(97).repeat(i - 2);
+    nextAttempt += String.fromCharCode(97).repeat(i - 2);
   }
   if (i > 1) {
-    ret += String.fromCharCode(97 + (firstCharIncr - 123));
+    nextAttempt += String.fromCharCode(97 + (firstCharIncr - 123));
   }
-  return ret;
+  return nextAttempt;
 }
 
+/**
+ * Start guessing at strings.
+ */
 function start() {
   console.log(`Worker ${workerId} trying to guess ${hash}`);
+  // Set start time for calculating speed.
   const startTime = new Date();
-  // Start on first character, plus the number of this worker.
+  // Start on 'a' character, plus the number of this worker.
   let attempt = String.fromCharCode(97 + workerId);
+  // Tracker records the number of guesses made so far, for speed calculation.
   let tracker = 0;
+  // Run until the worker is terminated.
   while (true) {
-    attempt = increment(attempt);
     if (SparkMD5.hash(attempt) === hash) {
+      // The guess matched! Tell the main script.
       self.postMessage({
         type: 'cracked',
         content: attempt,
@@ -67,10 +80,16 @@ function start() {
         content: tracker / ((new Date() - startTime) / 1000),
       });
     }
+    // Get next guess and increment count of guesses.
+    attempt = increment(attempt);
     tracker++;
   }
 }
 
+/**
+ * Listen to messages from the main script.
+ * @param {Object}    message   Message object from client.js.
+ */
 self.addEventListener('message', msg => {
   switch (msg.data.type) {
     case 'setWorkerId':
